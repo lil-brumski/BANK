7306e61345f01c1143fe82b53bff711d02827084
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-06-03 20:54:30 +0100

    Update bank.hpp

--- bank.hpp
+++ bank.hpp
@@ -31,46 +31,46 @@ std::string CONVERT(double value){
     // Create an output string stream
     std::ostringstream oss;
     // Set fixed format and precision to 2 decimal places
-    oss << std::fixed << std::setprecision(0) << value;
+    oss << std::fixed << std::setprecision(2) << value;
     // Convert the stream to a string
     std::string str = oss.str();
 
-   if(str.length() >= 4 && str.length() <= 9){
-             if(str.length() == 4){
+   if(str.length() >= 7 && str.length() <= 12){
+             if(str.length() == 7){
                  str.insert(1, ",");
-                 }
-             else if(str.length() == 5){
-                 str.insert(2, ",");
-                 }
-             else if(str.length() == 6){
-                 str.insert(3, ",");
-                 }
-             else if(str.length() == 7){
-                 str.insert(1, ",");
-                 str.insert(5, ",");
                  }
              else if(str.length() == 8){
                  str.insert(2, ",");
-                 str.insert(6, ",");
                  }
              else if(str.length() == 9){
+                 str.insert(3, ",");
+                 }
+             else if(str.length() == 10){
+                 str.insert(1, ",");
+                 str.insert(5, ",");
+                 }
+             else if(str.length() == 11){
+                 str.insert(2, ",");
+                 str.insert(6, ",");
+                 }
+             else if(str.length() == 12){
                  str.insert(3, ",");
                  str.insert(7, ",");
                  }
        }
        
-   else if(str.length() >= 10 && str.length() <= 12){
-             if(str.length() == 10){
+   else if(str.length() >= 13 && str.length() <= 15){
+             if(str.length() == 13){
                  str.insert(1, ",");
                  str.insert(5, ",");
                  str.insert(9, ",");
                  }
-             else if(str.length() == 11){
+             else if(str.length() == 14){
                  str.insert(2, ",");
                  str.insert(6, ",");
                  str.insert(10, ",");
                  }
-             else if(str.length() == 12){
+             else if(str.length() == 15){
                  str.insert(3, ",");
                  str.insert(7, ",");
                  str.insert(11, ",");
@@ -95,19 +95,16 @@ double FORMAT(std::string value){
     std::string cleanedValue;
     if(!value.empty()){
          for(int x = 0; x < value.length(); x++){
-               if(std::isdigit(value[x])){
+               if(std::isdigit(value[x]) ||  value[value.length() - 2] == '.'){
                cleanedValue += value[x];
                }
          }
+         //cleanedValue += ".00";
     }
     else{
-        cleanedValue = "0";
+        cleanedValue = "0.00";
     }
-        
-    /*char charToRemove = ',';    
-    
-    value.erase(std::remove(value.begin(), value.end(), charToRemove), value.end());*/
-    
+           
     return std::stod(cleanedValue);
 }
 
@@ -115,7 +112,7 @@ double FORMAT(std::string value){
 double BALANCE(double cash){
     static double balance = 0;
     balance += cash;
-    balance = std::round(balance);
+std::cout<<std::fixed<<std::setprecision(2)<<std::endl;
     return balance;
 }
 #endif
